3a76e34be8033450966e96709d7371fa6fb6f2a1
Sonu <sonu@mindrops.com>
2024-11-06 12:33:54 +0530

    add header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { SidebarModule } from 'primeng/sidebar';
-import { ButtonModule } from 'primeng/button';  // Optional if you need buttons
-import { MenubarModule } from 'primeng/menubar'; 
+import { ButtonModule } from 'primeng/button'; // Optional if you need buttons
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,
-
-    SidebarModule,
-    ButtonModule,
-    MenubarModule,
-    CommonModule
-  ],
+  imports: [RouterOutlet, ButtonModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-
-  
-  isHovered = false;
-  isExpanded = false;
-  isVisible = true;  // To keep the sidebar visible by default
-
-  onMouseEnter() {
-    this.isHovered = true;
-    this.isExpanded = true;
-  }
-
-  onMouseLeave() {
-    this.isHovered = false;
-    this.isExpanded = false;
-  }
-}
+export class AppComponent {}
