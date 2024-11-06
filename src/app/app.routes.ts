870021117daba592ea4a7bb99f3a325beed6823a
Sonu <sonu@mindrops.com>
2024-11-06 12:34:10 +0530

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./components/landing/landing.component').then(m => m.LandingComponent)
+
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./components/landing/landing.component').then(m => m.LandingComponent)
+    }
+];
