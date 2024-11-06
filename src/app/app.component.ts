52f195b4ca3b09bbc76707154be3ddcdf2228419
Sonu <sonu@mindrops.com>
2024-11-06 12:33:37 +0530

    add basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';  // Optional if you need buttons
+import { MenubarModule } from 'primeng/menubar'; 
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+
+    SidebarModule,
+    ButtonModule,
+    MenubarModule,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'portfolio';
+
+  
+  isHovered = false;
+  isExpanded = false;
+  isVisible = true;  // To keep the sidebar visible by default
+
+  onMouseEnter() {
+    this.isHovered = true;
+    this.isExpanded = true;
+  }
+
+  onMouseLeave() {
+    this.isHovered = false;
+    this.isExpanded = false;
+  }
 }
