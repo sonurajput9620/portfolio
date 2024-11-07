c106a6cbdd2fe67e7865757a1e3305206a0b92cd
Sonu <sonu@mindrops.com>
2024-11-07 11:12:46 +0530

    add router in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 import { ButtonModule } from 'primeng/button'; // Optional if you need buttons
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, CommonModule],
+  imports: [RouterOutlet, ButtonModule, CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
