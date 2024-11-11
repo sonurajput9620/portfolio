1022ca4053a4b1763679cdba0d342b0868310423
Sonu <sonu@mindrops.com>
2024-11-11 16:04:13 +0530

    add resume and project content

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { ProjectsComponent } from '../projects/projects.component';
+import { ScrollTopModule } from 'primeng/scrolltop';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [],
+  imports: [ProjectsComponent, ScrollTopModule, CommonModule],
   templateUrl: './resume.component.html',
-  styleUrl: './resume.component.scss'
+  styleUrl: './resume.component.scss',
 })
-export class ResumeComponent {
-
-}
+export class ResumeComponent {}
