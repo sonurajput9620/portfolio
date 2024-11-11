71522c03545632bdb8c8d71899071a9e0988f3a7
Sonu <sonu@mindrops.com>
2024-11-11 16:02:17 +0530

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,17 @@ import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button'; // Optional if you need buttons
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, CommonModule, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
