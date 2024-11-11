96116875a20e31351880343612e48bf9e1ec45aa
Sonu <sonu@mindrops.com>
2024-11-11 16:03:25 +0530

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,43 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: '',
-        loadComponent: () => import('./components/landing/landing.component').then(m => m.LandingComponent)
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./components/landing/landing.component').then(
+        (m) => m.LandingComponent
+      ),
+  },
+  {
+    path: 'resume',
+    loadComponent: () =>
+      import('./components/resume/resume.component').then(
+        (m) => m.ResumeComponent
+      ),
+  },
 
-    },
-    {
-        path: 'resume',
-        loadComponent: () => import('./components/resume/resume.component').then(m => m.ResumeComponent)
-
-    },
-    {
-        path: '**',
-        loadComponent: () => import('./components/landing/landing.component').then(m => m.LandingComponent)
-    }
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./components/contact/contact.component').then(
+        (m) => m.ContactComponent
+      ),
+  },
+  {
+    path: 'projects',
+    loadComponent: () =>
+      import('./components/projects/projects.component').then(
+        (m) => m.ProjectsComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
 ];
